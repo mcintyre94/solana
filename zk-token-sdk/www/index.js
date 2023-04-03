23c573f550d6d321051b9a823779ecd5972b902f
Callum McIntyre <mcintyre1994@gmail.com>
2023-04-03 15:21:00 +0100

    Add from_uint8_array and convert bytes back to pubkey

--- zk-token-sdk/www/index.js
+++ zk-token-sdk/www/index.js
@@ -8,3 +8,9 @@ console.log({ publicKey });
 
 const bytes = publicKey.to_uint8_array();
 console.log({ bytes });
+
+const recovered = zk.ElGamalPubkey.from_uint8_array(bytes);
+console.log({ recovered });
+
+const recoveredBytes = recovered.to_uint8_array();
+console.log({ recoveredBytes });
